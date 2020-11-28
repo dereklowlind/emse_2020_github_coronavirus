93106befc350d00b0370192e0051c7ea42357d1b
dereklowlind <dereklowlind@gmail.com>
2020-11-28 12:18:27 -0800

    rewrite of repo_metrics.py to use a single commit history file and metrics by year and month

--- repo_metrics.py
+++ repo_metrics.py
@@ -1,37 +1,39 @@
 import csv
 import dateparser # can probably find a faster method to do this
+import datetime
+
+def metrics(year, month, commit_history):
+    timeframe = []
+    previous_history = []
+    for row in commit_history:
+        if row[-1].year == year and row[-1].month == month:
+            timeframe.append(row)
+        elif row[-1].year == year and month < month:
+            previous_history.append(row)
+        elif row[-1].year < year:
+            previous_history.append(row)
+
+
+    in_timeframe_devs = [x[2] for x in timeframe]
+    in_timeframe_devs_unique = set(in_timeframe_devs)
+    previous_history_devs = [x[2] for x in previous_history]
+    previous_history_devs_unique = set(previous_history_devs)
+    new_devs = in_timeframe_devs_unique.difference(previous_history_devs_unique)
+    return [len(in_timeframe_devs_unique), len(new_devs), len(timeframe)]
 
 N_of_active_devs = -1
 N_of_new_devs = -1
 N_of_commits = -1
 
-in_study_file = open("committed_in_study_period.csv")
-in_study = list(csv.reader(in_study_file))
-# conver the timestamp string into a datetime and append it to the end of the list
-for row in in_study:
+commit_history_file = open("committed_all_time.csv")
+commit_history = list(csv.reader(commit_history_file))
+# convert the timestamp string into a datetime and append it to the end of the list
+for row in commit_history:
+    # 'Thu Nov 26 13:29:19 2020 +0100' , date_formats=['%a %b %d %H:%M:%S %Y']
     row = row.append(dateparser.parse(row[-1]))
 
-pre_study_file = open("committed_pre_study_period.csv")
-pre_study = list(csv.reader(pre_study_file))
-for row in pre_study:
-    row = row.append(dateparser.parse(row[-1]))
 
-in_study_devs = [x[2] for x in in_study]
-in_study_devs_unique = set(in_study_devs)
-# print(in_study_devs_unique)
-N_of_active_devs = len(in_study_devs_unique)
-
-pre_study_devs = [x[2] for x in pre_study]
-pre_study_devs_unique = set(pre_study_devs)
-# print(pre_study_devs_unique)
-new_devs = in_study_devs_unique.difference(pre_study_devs_unique)
-N_of_new_devs = len(new_devs)
-
-print(N_of_active_devs)
-print(N_of_new_devs)
+print("done preprocessing")
 
 
-test = in_study[0:10]
-
-
-print(test)
+print(metrics(2019, 1, commit_history))
