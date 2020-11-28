22a3f3619eba2a836a67aa825ed76733e4dbe0d6
dereklowlind <dereklowlind@gmail.com>
2020-11-28 11:21:40 -0800

    modified get_java_100.py to pull 300 total responses

--- get_java_100.py
+++ get_java_100.py
@@ -1,10 +1,14 @@
 import requests
-
-url = "https://api.github.com/search/repositories?q=language:java&sort=stars&created:<2019-01-01&pushed:>2019-01-01&order=desc"
+import json
+url = "https://api.github.com/search/repositories?q=language:java&sort=stars&created:<2019-01-01&pushed:>2019-01-01&order=deschttps://api.github.com/repos/elastic/elasticsearch/branches?&per_page=100"
 
 payload={}
 headers = {}
 
-response = requests.request("GET", url, headers=headers, data=payload)
+first_hundred = requests.request("GET", url+'&page=1', headers=headers, data=payload)
+second_hundred = requests.request("GET", url+'&page=2', headers=headers, data=payload)
+third_hundred = requests.request("GET", url+'&page=3', headers=headers, data=payload)
 
-print(response.text)
+print(first_hundred.text)
+print(second_hundred.text)
+print(third_hundred.text)
